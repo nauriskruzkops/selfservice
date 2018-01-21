84c4320ef7dfb6bd9acd78f719e09c01980b8a86
Nauris Kruzkops <nauris.kruzkops@gmail.com>
2018-01-21 21:32:53 +0200

    Calendar - vocation form

--- src/Resources/views/form/date_widget.html.php
+++ src/Resources/views/form/date_widget.html.php
@@ -6,4 +6,12 @@ if (!empty(array_filter($value))) {
 }
 ?>
 
-<input class="form-control datepicker" data-provide="datepicker" data-date-format="yyyy-mm-dd" type="text" <?php echo $view['form']->block($form, 'widget_attributes') ?><?php if (!empty($value) || is_numeric($value)): ?> value="<?php echo $view->escape($value) ?>"<?php endif ?> />
+<input class="form-control" date-type="datepicker" type="text" <?php echo $view['form']->block($form, 'widget_attributes') ?><?php if (!empty($value) || is_numeric($value)): ?> value="<?php echo $view->escape($value) ?>"<?php endif ?> />
+<script type="text/javascript">
+    $(function () {
+        $('input#<?= $id?>').datetimepicker({
+            format: 'YYYY-MM-DD',
+            showClear : true,
+        });
+    })
+</script>
--- src/Resources/views/layout/base.html.php
+++ src/Resources/views/layout/base.html.php
@@ -15,13 +15,19 @@ use Symfony\Bundle\FrameworkBundle\Templating\PhpEngine;
         <meta name="description" content="">
         <meta name="author" content="">
         <title>Selfservice</title>
-        <link href="<?= $view['assets']->getUrl('/assets/vendor/bootstrap/css/bootstrap.min.css', 'vendor') ?>" rel="stylesheet">
+        <link href="<?= $view['assets']->getUrl('//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', 'vendor') ?>" rel="stylesheet">
         <link href="<?= $view['assets']->getUrl('/assets/vendor/font-awesome/css/font-awesome.min.css', 'vendor') ?>" rel="stylesheet" type="text/css">
         <link href="<?= $view['assets']->getUrl('/assets/theme/sb-admin.css', 'sbadmin') ?>" rel="stylesheet">
-        <link href="<?= $view['assets']->getUrl('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.css', 'css') ?>" rel="stylesheet">
+        <link href="<?= $view['assets']->getUrl('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css', 'css') ?>" rel="stylesheet">
         <link href="<?= $view['assets']->getUrl('/assets/css/custom-overwrite.css', 'css') ?>" rel="stylesheet">
+
         <link href="<?= $view['assets']->getUrl('/assets/css/calendar.css', 'css') ?>" rel="stylesheet">
         <link href="<?= $view['assets']->getUrl('/assets/css/calendar-timetable.css', 'css') ?>" rel="stylesheet">
+
+        <script src="<?= $view['assets']->getUrl('/assets/vendor/jquery/jquery.min.js', 'vendor') ?>"></script>
+        <script src="<?= $view['assets']->getUrl('/assets/vendor/popper.min.js', 'vendor') ?>"></script>
+        <script src="<?= $view['assets']->getUrl('/assets/vendor/bootstrap/js/bootstrap.min.js', 'vendor') ?>"></script>
+        <script src="<?= $view['assets']->getUrl('/assets/js/main.js', 'vendor') ?>"></script>
 
     </head>
     <body class="fixed-nav sticky-footer bg-dark" id="page-top">
@@ -36,26 +42,24 @@ use Symfony\Bundle\FrameworkBundle\Templating\PhpEngine;
             </div>
         </div>
         <!-- Global AJAX Modal:END -->
-        <script src="<?= $view['assets']->getUrl('/assets/vendor/jquery/jquery.min.js', 'vendor') ?>"></script>
-        <script src="<?= $view['assets']->getUrl('/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', 'vendor') ?>"></script>
-        <script src="<?= $view['assets']->getUrl('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js', 'vendor') ?>"></script>
-        <script src="<?= $view['assets']->getUrl('/assets/vendor/jquery-easing/jquery.easing.min.js', 'vendor') ?>"></script>
+
+        <script src="<?= $view['assets']->getUrl('//cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js', 'vendor') ?>"></script>
+        <script src="<?= $view['assets']->getUrl('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js', 'vendor') ?>"></script>
         <script src="<?= $view['assets']->getUrl('/assets/theme/sb-admin.js', 'sbadmin') ?>"></script>
         <script type="text/javascript">
-            (function () {
+            $(function () {
                 var globalAjaxModal = $('#globalAjaxModal');
                 globalAjaxModal.on('show.bs.modal', function(e) {
                     var globalAjaxModalUrl = $(e.relatedTarget).data('url');
                     $(this)
                         .addClass('modal-scrollfix')
-                        .find('.modal-body')
+                        .find('.modal-content')
                         .html('loading...')
                         .load(globalAjaxModalUrl, function() {
                             globalAjaxModal.removeClass('modal-scrollfix').modal('handleUpdate');
                         });
                 });
-
-            })();
+            });
         </script>
     </body>
 </html>
